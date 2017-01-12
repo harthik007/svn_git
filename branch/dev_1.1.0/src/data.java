a41baacdd9e2b210993e3d0d5bc70228e97af54d
jai <jai>
2017-01-12 05:21:40 +0000

    copying src code from trunk to branch
    git-svn-id: svn://ubuntu/idea@3 3435f4ba-c1a5-4c94-b893-a0c53768027d

---
+++ branch/dev_1.1.0/src/data.java
@@ -0,0 +1,14 @@
+public class data
+
+       {
+
+           public static void main(String[] args)
+
+             {
+
+                System.out.println("welcome to idea data plans");
+
+             }
+        }
+
+             
